2322193d2115977ce6dba6a4b44f47723b562405
Gangbros777 <161280210+Gangbros777@users.noreply.github.com>
2024-06-04 02:43:01 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+let firstNumber = +prompt("Birinchi rqamni yozing");
+let secondNumber = +prompt("Ikkinchi raqamni yozing");
+
+if (firstNumber === secondNumber) {
+  alert("Birinchi raqam = Ikkinchi raqam");
+} else if (firstNumber > secondNumber) {
+  alert("Birinchi raqam > Ikkinchi raqam");
+} else {
+  alert("Birinchi raqam < Ikkinchi raqam");
+}
